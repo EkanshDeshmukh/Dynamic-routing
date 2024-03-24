d44d8e4335eec3ad87c2182ed75d50c3945fc5ba
EkanshDeshmukh <yashudeshmukh25@email.com>
2024-03-24 17:56:06 +0530

    update home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ const Home = () => {
     return (
       <>
         <h1>This is  home page</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias veniam eius quaerat aliquam cupiditate fugit voluptatum qui non voluptas incidunt neque dolores ut, repellendus, quos asperiores nostrum dolor maiores obcaecati!</p>
       </>
     );
   };
